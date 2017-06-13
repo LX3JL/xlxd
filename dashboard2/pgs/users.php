12404e36b4099ffb15f63c59491df4b5b4d7a09d
phl0 <florian@florian-wolters.de>
2017-06-13 09:35:00 +0200

    Use CSS also for dashboard2

--- dashboard2/pgs/users.php
+++ dashboard2/pgs/users.php
@@ -26,7 +26,7 @@ for ($i=0;$i<$Reflector->StationCount();$i++) {
    
    list ($Flag, $Name) = $Reflector->GetFlag($Reflector->Stations[$i]->GetCallSign());
    if (file_exists("./img/flags/".$Flag.".png")) {
-      echo '<img src="./img/flags/'.$Flag.'.png" class="table-flag" alt="'.$Name.'" title="'.$Name.'">';
+      echo '<a href="#" class="tip"><img src="./img/flags/'.$Flag.'.png" class="table-flag" alt="'.$Name.'"><span>'.$Name.'</span></a>';
    }
    echo '</td>
    <td><a href="https://www.qrz.com/db/'.$Reflector->Stations[$i]->GetCallsignOnly().'" class="pl" target="_blank">'.$Reflector->Stations[$i]->GetCallsignOnly().'</a></td>

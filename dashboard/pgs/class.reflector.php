73be5ec01824fa5b34c8482b0bb8b607aa689ad2
LX1IQ <lx1iq@epf.lu>
2017-06-14 08:03:26 +0200

    bugfix
    removed error when prefix is not found in the country.csv

--- dashboard/pgs/class.reflector.php
+++ dashboard/pgs/class.reflector.php
@@ -258,6 +258,7 @@ class xReflector {
       $Image     = "";
       $FoundFlag = false;
       $Letters = 4;
+	  $Name = "";
       while (($Letters >= 2) && (!$FoundFlag)) {
          $j = 0;
          $Prefix = substr($Callsign, 0, $Letters);

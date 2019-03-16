3e8d910cb08db312c87745a1338fa9ebde9a21ac
LX3JL <lx3jl@alt.lu>
2019-03-16 15:56:54 +0100

    Revert "xlxd 2.2.3"
    This reverts commit 63a41d5c88745010ce8a84c142329a6a25058f02.

--- src/main.h
+++ src/main.h
@@ -49,7 +49,7 @@
 
 #define VERSION_MAJOR                   2
 #define VERSION_MINOR                   2
-#define VERSION_REVISION                3
+#define VERSION_REVISION                2
 
 // global ------------------------------------------------------
 

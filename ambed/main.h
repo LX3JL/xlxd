df41211a11c77c248c4a42b4568d0a6db55f5d30
LX3JL <lx3jl@alt.lu>
2019-10-22 19:45:20 +0200

    ambed 1.3.4
    Solved issue when pairing a 3003 with a 3000.
    Solved issue when aggregating more than 2 devices

--- ambed/main.h
+++ ambed/main.h
@@ -49,7 +49,7 @@
 
 #define VERSION_MAJOR                   1
 #define VERSION_MINOR                   3
-#define VERSION_REVISION                3
+#define VERSION_REVISION                4
 
 // global ------------------------------------------------------
 

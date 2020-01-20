66337269674f7c70a509afa114b73b130e4fdc81
Geoffrey Merck <F4FXL@users.noreply.github.com>
2020-01-20 19:40:32 +0100

    Add DMR Suffix

--- src/cdmriddirfile.cpp
+++ src/cdmriddirfile.cpp
@@ -128,6 +128,7 @@ bool CDmridDirFile::RefreshContent(const CBuffer &buffer)
                     // new entry
                     uint32 ui = atoi(dmrid);
                     CCallsign cs(callsign, ui);
+                    cs.SetSuffix("DMR");
                     if ( cs.IsValid() )
                     {
                         m_CallsignMap.insert(std::pair<uint32,CCallsign>(ui, cs));
--- src/cdmriddirhttp.cpp
+++ src/cdmriddirhttp.cpp
@@ -70,6 +70,7 @@ bool CDmridDirHttp::RefreshContent(const CBuffer &buffer)
                     // new entry
                     uint32 ui = atoi(dmrid);
                     CCallsign cs(callsign, ui);
+                    cs.SetSuffix("DMR");
                     if ( cs.IsValid() )
                     {
                         m_CallsignMap.insert(std::pair<uint32,CCallsign>(ui, cs));

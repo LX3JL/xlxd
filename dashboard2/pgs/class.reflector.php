c22c7f01e6633408a1b5d1e9c36be855552820fa
LX1IQ <lx1iq@epf.lu>
2021-01-18 11:54:28 +0100

    Update class.reflector.php

--- dashboard2/pgs/class.reflector.php
+++ dashboard2/pgs/class.reflector.php
@@ -49,12 +49,6 @@ class xReflector {
             return false;
          }
          
-#         $this->ServiceName = substr($this->XMLContent, strpos($this->XMLContent, "<XLX")+4, 3);        
-#         if (!is_numeric($this->ServiceName)) {
-#            $this->ServiceName = null;
-#            return false;
-#         }  
-#         
          $this->ReflectorName = "XLX".$this->ServiceName;
          
          $LinkedPeersName = "XLX".$this->ServiceName."  linked peers";

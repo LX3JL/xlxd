264c78203cdce6dab1e156424ec84e1981a7938b
LX1IQ <lx1iq@epf.lu>
2021-07-06 09:32:36 +0200

    db 2.4.2

--- dashboard/pgs/config.inc.php
+++ dashboard/pgs/config.inc.php
@@ -17,7 +17,7 @@ $VNStat      = array();
 
 $PageOptions['ContactEmail']                         = 'your_email';	// Support E-Mail address
 
-$PageOptions['DashboardVersion']                     = '2.4.1';		// Dashboard Version
+$PageOptions['DashboardVersion']                     = '2.4.2';		// Dashboard Version
 
 $PageOptions['PageRefreshActive']                    = true;		// Activate automatic refresh
 $PageOptions['PageRefreshDelay']                     = '10000';		// Page refresh time in miliseconds

f8fb0c6fde451d72cb7e6bcbb77a7d933ade9f10
Andy Taylor <andy@mw0mwz.co.uk>
2025-11-22 21:24:19 +0000

    Additional security headders to improve security score

--- dashboard1/pgs/config.inc.php
+++ dashboard1/pgs/config.inc.php
@@ -17,7 +17,7 @@ $VNStat      = array();
 
 $PageOptions['ContactEmail']                         = 'your_email';	// Support E-Mail address
 
-$PageOptions['DashboardVersion']                     = '2.4.3';		// Dashboard Version
+$PageOptions['DashboardVersion']                     = '2.4.4';		// Dashboard Version
 
 $PageOptions['PageRefreshActive']                    = true;		// Activate automatic refresh
 $PageOptions['PageRefreshDelay']                     = '10000';		// Page refresh time in miliseconds
